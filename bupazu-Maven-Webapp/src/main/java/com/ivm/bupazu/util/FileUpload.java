6509e5b3d46a6c8b871cc4e82996c780a8706812
Administrator <390824345@qq.com>
2015-08-08 13:44:34 +0800

    sfs

--- bupazu-Maven-Webapp/src/main/java/com/ivm/bupazu/util/FileUpload.java
+++ bupazu-Maven-Webapp/src/main/java/com/ivm/bupazu/util/FileUpload.java
@@ -5,8 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.File;  
 import java.io.FileOutputStream;
 import java.io.IOException;  
-import java.util.Date;  
-import java.util.Properties;
 
 public class FileUpload {  
 	  
@@ -15,9 +13,9 @@ public class FileUpload {
         // 根据配置文件获取服务器图片存放路径
         String picDir = "";
         //这里封装了读取配置文件的方法 配置文件中有图片的存放地址和获取地址
-        	PropertiesUtil util=new PropertiesUtil("pictrueUpload.properties");
-        	util.printProperties() ;
-            picDir = util.getProperty("YongHuTuXiang");
+        PropertiesUtil util= new PropertiesUtil("pictrueUpload.properties");
+        util.printProperties() ;
+        picDir = util.getProperty("YongHuTuXiang");
             String saveFilePath = picDir; 
         /* 构建文件目录 */
         File fileDir = new File(saveFilePath);
