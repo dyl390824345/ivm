1dba4f53d6de4bbaefa63873cec5d0ff35e0dc84
Administrator <390824345@qq.com>
2015-08-09 22:37:55 +0800

    以@value注解从配置文件中读取图像存储路径

--- bupazu-Maven-Webapp/src/main/java/com/ivm/bupazu/util/FileUpload.java
+++ bupazu-Maven-Webapp/src/main/java/com/ivm/bupazu/util/FileUpload.java
@@ -1,5 +1,6 @@
 package com.ivm.bupazu.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;    
 import javax.servlet.http.HttpServletRequest;  
 import java.io.File;  
@@ -8,15 +9,24 @@ import java.io.IOException;
 
 public class FileUpload {  
 	  
-    private void saveFile(String newFileName, MultipartFile filedata) throws IOException {
+	 @Value(value = "${YongHuTuXiang}") 
+	 private  String YongHuTuXiang;  
+	      
+	    public void setPicUrl(String YongHuTuXiang) {  
+	        this.YongHuTuXiang = YongHuTuXiang;  
+	    }  
+	
+	private void saveFile(String newFileName, MultipartFile filedata) throws IOException {
         // TODO Auto-generated method stub
         // 根据配置文件获取服务器图片存放路径
-        String picDir = "";
-        //这里封装了读取配置文件的方法 配置文件中有图片的存放地址和获取地址
-        PropertiesUtile util= new PropertiesUtile("pictrueUpload.properties");
-        util.printProperties() ;
-        picDir = util.getProperty("YongHuTuXiang");
-        String saveFilePath = picDir; 
+    	
+//    	
+//    	String picDir = "";
+//        //这里封装了读取配置文件的方法 配置文件中有图片的存放地址和获取地址
+//        PropertiesUtile util= new PropertiesUtile("pictrueUpload.properties");
+//        util.printProperties() ;
+//        picDir = util.getProperty("YongHuTuXiang");
+       String saveFilePath = YongHuTuXiang; 
         /* 构建文件目录 */
         File fileDir = new File(saveFilePath);
         if (!fileDir.exists()) {
